7fb45997c9830594f861879d566f0de901819cf1
Ben Hadley <bahadley@soe.ucsc.edu>
2016-05-07 12:08:56 -0700

    Added json struct

--- operator/window.go
+++ operator/window.go
@@ -1,15 +1,34 @@
 package operator
 
 import (
+    "encoding/json"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/bahadley/esp/log"
 )
 
-func Window(ingest chan string) {
+type (
+	gMsg struct {
+		Sensor     string     `json:"sensor"`
+		Type       string     `json:"type"`
+        Timestamp  time.Time  `json:"ts"`
+        Data       float64    `json:"data"`
+	}
+)
 
+func Window(ingest chan string) {
 	for {
 		msg := <-ingest
-		log.Logoutput(log.InfoPrefix, fmt.Sprintf("Windowed: %s", msg))
+		var gm gMsg
+        err := json.NewDecoder(strings.NewReader(msg)).Decode(&gm)
+        if err != nil {
+			log.Logoutput(log.ErrPrefix, err.Error())
+			continue
+        }
+		log.Logoutput(log.InfoPrefix, 
+			fmt.Sprintf("Windowed: %s,%s,%s,%f", gm.Sensor, 
+				gm.Type, gm.Timestamp, gm.Data))
 	}
 }
