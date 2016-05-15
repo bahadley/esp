b84cb47363b20080fe5372bfc26369c2ac47e19a
Ben Hadley <bahadley@soe.ucsc.edu>
2016-05-15 11:29:33 -0700

    Added configuration of window capacity and trigger values from environment variables

--- operator/window.go
+++ operator/window.go
@@ -1,8 +1,25 @@
 package operator
 
+import (
+	"os"
+	"strconv"
+
+	"github.com/bahadley/esp/log"
+)
+
+const (
+	defaultCapacity   = 4
+	defaultTriggerQty = 2
+
+	envWinCap  = "ESP_WINDOW_CAPACITY"
+	envWinTrig = "ESP_WINDOW_TRIGGER"
+)
+
 var (
-	window []SensorTuple
-	count  int
+	window    []SensorTuple
+	count     int
+	trigger   int
+	avgFactor float64
 )
 
 func AppendTuple(st SensorTuple) (float64, bool) {
@@ -17,13 +34,37 @@ func AppendTuple(st SensorTuple) (float64, bool) {
 	}
 
 	count++
-	if count == 2 {
+	if count == trigger {
 		count = 0
-		return (window[0].Data + window[1].Data) / 2.0, true
+		return (window[0].Data + window[1].Data) / avgFactor, true
 	}
 	return 0.0, false
 }
 
 func init() {
-	window = make([]SensorTuple, 4)
+	var err error
+	var cap int
+
+	envVal := os.Getenv(envWinCap)
+	if len(envVal) == 0 {
+		cap = defaultCapacity
+	} else {
+		cap, err = strconv.Atoi(envVal)
+		if err != nil {
+			log.Error.Fatalf("Invalid environment variable: %s", envWinCap)
+		}
+	}
+
+	envVal = os.Getenv(envWinTrig)
+	if len(envVal) == 0 {
+		trigger = defaultTriggerQty
+	} else {
+		trigger, err = strconv.Atoi(envVal)
+		if err != nil {
+			log.Error.Fatalf("Invalid environment variable: %s", envWinTrig)
+		}
+	}
+
+	window = make([]SensorTuple, cap)
+	avgFactor = float64(trigger)
 }
