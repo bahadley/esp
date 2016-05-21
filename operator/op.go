1e621252daee4bd229b66b6e6eef7871ebd653cb
Ben Hadley <bahadley@soe.ucsc.edu>
2016-05-21 10:43:03 -0700

    Removed critical section

--- operator/op.go
+++ operator/op.go
@@ -1,32 +1,23 @@
 package operator
 
 import (
-	"sync"
-
 	"github.com/bahadley/esp/system"
 )
 
 var (
 	IngestChan chan []byte
 	EgressChan chan []byte
-
-	// Used for IngestChan write critical section.
-	ingestMutex sync.Mutex
 )
+
+func QueueMsg(msg []byte) {
+	IngestChan <- msg
+}
 
 func Ingest() {
 	for {
 		msg := <-IngestChan
 		windowInsert(msg)
 	}
-}
-
-func QueueMsg(msg []byte) {
-	ingestMutex.Lock()
-	{
-		IngestChan <- msg
-	}
-	ingestMutex.Unlock()
 }
 
 func init() {
